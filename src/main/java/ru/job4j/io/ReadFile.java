67bb1daf0c3b59f7f2cc20024a538ad34307aa41
SergeyMikhalev <tolltroll@yandex.ru>
2022-10-13 10:06:46 +0300

    thread safe

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,6 +1,9 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.function.Predicate;
 
 public final class ReadFile {
@@ -10,25 +13,25 @@ public final class ReadFile {
         this.file = file;
     }
 
-    public String getContent() throws IOException {
-        return getFilteredContent((Integer i) -> true);
+    public synchronized String getContent() throws IOException {
+        return getFilteredContent(i -> true);
     }
 
-    public String getContentWithoutUnicode() throws IOException {
-        return getFilteredContent((Integer i) -> i < 0x80);
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        return getFilteredContent(i -> i < 0x80);
     }
 
     private String getFilteredContent(Predicate<Integer> filter) throws IOException {
-        String output = "";
-        try (InputStream i = new FileInputStream(file)) {
+        StringBuilder output = new StringBuilder();
+        try (BufferedInputStream i = new BufferedInputStream(new FileInputStream(file))) {
             int data;
-            while ((data = i.read()) > 0) {
+            while ((data = i.read()) != -1) {
                 if (filter.test(data)) {
-                    output += (char) data;
+                    output.append((char) data);
                 }
             }
         }
-        return output;
+        return output.toString();
     }
 
 }
--- src/main/java/ru/job4j/io/WriteFile.java
+++ src/main/java/ru/job4j/io/WriteFile.java
@@ -1,9 +1,9 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public final class WriteFile {
     private final File file;
@@ -12,8 +12,8 @@ public final class WriteFile {
         this.file = file;
     }
 
-    public void saveContent(String content) throws IOException {
-        try (OutputStream o = new FileOutputStream(file)) {
+    public synchronized void saveContent(String content) throws IOException {
+        try (BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
             for (int i = 0; i < content.length(); i += 1) {
                 o.write(content.charAt(i));
             }
