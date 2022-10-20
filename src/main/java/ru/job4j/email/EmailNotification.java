50913ffa83408c2640932460401c317e637a4f87
SergeyMikhalev <tolltroll@yandex.ru>
2022-10-20 12:36:43 +0300

    Email

--- src/main/java/ru/job4j/email/EmailNotification.java
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-    ExecutorService pool =
+    private final ExecutorService pool =
             Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
@@ -18,5 +18,12 @@ public class EmailNotification {
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
