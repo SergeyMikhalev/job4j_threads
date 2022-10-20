5e387aef6b77242514cc8cb76e7d860a8805ed55
SergeyMikhalev <tolltroll@yandex.ru>
2022-10-20 12:25:10 +0300

    Email

---
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -0,0 +1,22 @@
+package ru.job4j.email;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    ExecutorService pool =
+            Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        String subject = "Notification " + user.getUsername() + " to email " + user.getEmail();
+        String body = "Add a new event to " + user.getUsername();
+        pool.submit(() -> send(subject, body, user.getEmail()));
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+}
